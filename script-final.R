28ccc360d13559be8f188ad043174119e5bc8b62
Claudiu Forgaci <claudiuforgaci@gmail.com>
2023-02-28 23:06:30 +0100

    Update script and add ua data

--- script-final.R
+++ script-final.R
@@ -16,6 +16,7 @@ library(ggrepel)
 ## Source: https://ec.europa.eu/eurostat/databrowser/view/URB_LPOP1__custom_5120358/default/table?lang=en
 pop_13_22 <- read_csv("data/raw/urb_lpop1__custom_5120358_page_linear.csv")
 pop_13_22_c <- read_csv("data/raw/urb_cpop1_page_linear.csv")
+ua <- read_csv("data/raw/URAU_LB_2021_3035_CITIES_within_frame.csv")
 
 # Examine the data ----
 # View(pop_13_22)
@@ -28,13 +29,14 @@ pop_13_22_long <- pop_13_22 |>
   # Remove countries
   filter(nchar(cities) > 2)
 
-# Show the population trend of all cities in the region
+# Show the population trend of all capital cities in the region
 pop_13_22_long |>
   # filter(cities %in% c("BE001F", "DE001F", "NL001F", "UK001F")) |> 
   filter(substr(cities, 3, 6) == "001F") |> 
+  left_join(ua |> filter(CITY_CPTL == "Y"), by = c("cities" = "FUA_CODE")) |> 
   group_by(cities) |> 
-  mutate(label = if_else(TIME_PERIOD == max(TIME_PERIOD), as.character(cities), NA_character_)) |> 
-  ungroup() |> 
+  mutate(label = if_else(TIME_PERIOD == max(TIME_PERIOD), as.character(URAU_NAME), NA_character_)) |> 
+  ungroup() |>
   ggplot(aes(x = TIME_PERIOD, y = OBS_VALUE, color = cities, group = cities)) +
   geom_line() +
   geom_label_repel(aes(label = label),
@@ -46,7 +48,8 @@ pop_13_22_long |>
                      limits = c(2013, 2023)) +
   xlab("Year") +
   ylab("Number of inhabitants") +
-  labs(title = "Number of inhabitants in the region") +
+  labs(title = "Population dynamics in the region",
+       subtitle = "Eurostat population data on FUA level between 2013-2021") +
   scale_color_discrete(name = "Muncipality") +
   theme(legend.position = "none")
 
